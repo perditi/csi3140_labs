c858a56b1e293806e1b45920e342aa910605a578
Timothy Mao <tmao042@uottawa.ca>
2024-05-27 16:04:12 -0400

    stupid move left and right, onclick rn moves left. idk bro.

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -1,14 +1,24 @@
-var nonpacman = [".", ""]
+var pacman;
+var fruit;
+var ghost;
+
+function showArray(arr){
+    var output = "[";
+    for (var i=0, len = arr.length; i < len; i++) {
+    output += arr[i] + ", ";
+    }
+    output += "]<br>";
+}
 
 function createGame(n){
     var game = new Array(n);
     
-    var pacman = Math.floor(n/2);
+    pacman = Math.floor(n/2);
     game[pacman] = "C"
-    var fruit = Math.floor(Math.random() * n);
+    fruit = Math.floor(Math.random() * n);
     if (game[fruit] == "C"){fruit += 1;}
     game[fruit] = "@";
-    var ghost = n;
+    ghost = n;
     game[ghost] = "^";
     for (var i = 0, len = game.length; i < len; i++){
         if (game[i] == null){
@@ -18,7 +28,34 @@ function createGame(n){
     return game;
 }
 
+function moveLeft(game){
+    game[pacman] = "";
+    if (pacman == 0){
+        pacman = game.length-1;
+    } else {
+        pacman = pacman - 1;
+    }
+    game[pacman] = "C" + game[pacman];
+    document.getElementById("test").innerHTML = game;
+}
+
+function moveRight(game){
+    game[pacman] = "";
+    if (pacman == game.length-1){
+        pacman = 0;
+    } else {
+        pacman = pacman + 1;
+    }
+    game[pacman] = "C" + game[pacman];
+    document.getElementById("test").innerHTML = game;
+}
 
 
 
-document.writeln(createGame(10));
+var game = createGame(10);
+document.getElementById("test").innerHTML = game;
+
+
+document.onclick = function(){moveLeft(game)};
+
+
