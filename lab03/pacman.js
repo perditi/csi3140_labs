8b290403ba43b3305a79425d1339b8bbf86e5f20
Timothy Mao <tmao042@uottawa.ca>
2024-06-03 17:30:50 -0400

    score + hiscore, ghost and fruit detection, pellets stored differently. ghost still doesn't move. game end and cont. implemented.

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -2,73 +2,194 @@ var pacman;
 var fruit;
 var ghost;
 
-function showArray(arr){
-    var output = "[";
-    for (var i=0, len = arr.length; i < len; i++) {
-    output += arr[i] + ", ";
-    }
-    output += "]<br>";
-}
+var high_score = 0;
 
-function createGame(n){
-    var game = new Array(n);
+var score;
+var fruitTimer = 0;
+
+var gameActive = false;
+var game;
+var pellets;
+
+function createGame(n, cont){
+    game = new Array(n);
+    pellets = new Array(n+1);
+    for (var i = 0; i < n+1; i++){
+        if (i == n){
+            pellets[i] = n-2;//stores total number
+        } else {
+            pellets[i] = 1;
+        }
+    }
     
-    pacman = Math.floor(n/2);
+    if (!cont){
+        pacman = Math.floor(n/2);
+        score = 0;
+    }
+    pellets[pacman] = 0;
+
     game[pacman] = "C"
-    fruit = Math.floor(Math.random() * n);
-    if (game[fruit] == "C"){fruit += 1;}
+
+    do {
+        fruit = Math.floor(Math.random() * (n));
+    } while (fruit == pacman);
     game[fruit] = "@";
-    ghost = n;
+    pellets[fruit] = 0;
+
+    do {
+        ghost = Math.floor(Math.random() * (n));
+    } while (ghost == pacman || ghost == fruit);
     game[ghost] = "^";
+
     for (var i = 0, len = game.length; i < len; i++){
         if (game[i] == null){
             game[i] = ".";
         }
     }
-    return game;
+    console.log(pellets);
+    displayScore();
+    fruitTimer = 0;
+    gameActive = true;
 }
 
-function moveLeft(game){
-    game[pacman] = "";
+function moveLeft(){
+    game[pacman] = "_";
     if (pacman == 0){
         pacman = game.length-1;
     } else {
         pacman = pacman - 1;
     }
-    game[pacman] = "C" + game[pacman];
-    document.getElementById("test").innerHTML = game;
+    if (game[pacman] == "_"){
+        game[pacman] = "C";
+    } else {
+        game[pacman] = "C" + game[pacman];
+    }
 }
 
-function moveRight(game){
-    game[pacman] = "";
+function moveRight(){
+    game[pacman] = "_";
     if (pacman == game.length-1){
         pacman = 0;
     } else {
         pacman = pacman + 1;
     }
-    game[pacman] = "C" + game[pacman];
-    document.getElementById("test").innerHTML = game;
+    if (game[pacman] == "_"){
+        game[pacman] = "C";
+    } else {
+        game[pacman] = "C" + game[pacman];
+    }
 }
 
-
-
-var game = createGame(10);
-document.getElementById("test").innerHTML = game;
-
-/*
-document.onkeydown = function(){
-    console.log("fuck");
-    if (KeyboardEvent.keyCode == 39){
-        moveRight(game);
-    } else if (KeyboardEvent.keyCode == 37){
-        moveLeft(game);
+function processMove(){
+    if (pacman == ghost){
+        console.log("ghost detected");
+        if (fruitTimer > 0){
+            game[pacman] = "C";
+            addScore(300);
+        } else {
+            gameActive = false;
+            game[pacman] = "^";
+        }
     }
-};*/
+
+    if (gameActive){
+        if (pellets[pacman] == 1){
+            console.log("pellet monch");
+            pellets[pacman] = 0;
+            pellets[pellets.length-1] -= 1;
+            console.log(pellets)
+            game[pacman] = "C";
+            addScore(100);
+        } else if (pacman == fruit){
+            console.log("fruit detected");
+            fruit = null;
+            game[pacman] = "C";
+            addScore(200);
+            fruitTimer = 15;
+        }
+    }
+
+    updateGame();
+}
+
+function addScore(n){
+    console.log("adding score");
+    score += n;
+    displayScore();
+}
+
+function displayScore(){
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+function updateGame(){
+    console.log("updategame call");
+    if (gameActive == false){
+        var tempGame = game;
+        display(tempGame);
+        console.log("checkpoint");
+        game = null;
+        document.getElementById("gamelose").innerHTML = "You lost! Restarting in three seconds...";
+        if (score > high_score){
+            high_score = score;
+            console.log("updating hiscore");
+            document.getElementById("hiscore").innerHTML = "High Score: " + high_score;
+        }
+        sleep(3000);
+        document.getElementById("gamelose").innerHTML = "";
+        createGame(10, false);
+        displayScore();
+    }
+    if (pellets[pellets.length-1] == 0){
+        createGame(10, true);
+    }
+    display(game);
+}
+
+function display(g){
+    var output = "[";
+    for (var i = 0, len = g.length; i < len; i++){
+        let cell = " " + g[i] + " ";
+        output += cell;
+        if (i == len-1){
+            output += "]";
+        }
+    }
+    if (fruitTimer > 0){
+        output += " @";
+    }
+    document.getElementById("game_screen").innerHTML = output;
+
+}
+
+function sleep(milliseconds) {
+    console.log("waiting " + milliseconds + " ms");
+    let timeStart = new Date().getTime();
+        while (true) {
+            let elapsedTime = new Date().getTime() - timeStart;
+            if (elapsedTime > milliseconds) {
+                console.log("done waiting");
+                break;
+            }
+    }
+}
+
+createGame(10, false)
+updateGame();
+display(game);
+
 
 document.addEventListener('keydown', function (e) {
-    if (e.key == "ArrowRight"){
-        moveRight(game);
-    } else if (e.key == "ArrowLeft"){
-        moveLeft(game);
+    if (gameActive){
+        if (e.key == "ArrowRight"){
+            console.log("right press");
+            moveRight();
+            processMove();
+        } else if (e.key == "ArrowLeft"){
+            console.log("left press");
+            moveLeft();
+            processMove();
+        }
+        console.log("pacman in " + pacman);
     }
   }, false);
